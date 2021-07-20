327773264b67accd3f394b433ecf4e48ebdfb6ea
Lucas Neves <lucas54neves@gmail.com>
2021-07-20 13:57:23 -0300

    update urls
    Add asset route

--- src/core/urls.py
+++ src/core/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from assets import views as viewsets
+
+route = routers.DefaultRouter()
+
+route.register('assets', viewsets.AssetViewSet, basename='Assets')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(route.urls))
 ]
