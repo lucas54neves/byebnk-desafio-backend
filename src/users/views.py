73932eb53f136e8669cc865c01280f8d3508b125
Lucas Neves <lucas54neves@gmail.com>
2021-07-22 02:16:57 -0300

    update balance

--- src/users/views.py
+++ src/users/views.py
@@ -1,9 +1,16 @@
 from rest_framework import viewsets
-from users import serializers, models
 from django.http import HttpResponse, JsonResponse
+from users import serializers, models
 
 class UserViewSet(viewsets.ModelViewSet):
   def balance(self):
-    response_data = {}
-    response_data['balance'] = self.user.balance
-    return JsonResponse(response_data)
+    try:
+      response_data = {}
+      response_data['balance'] = self.user.balance
+      return JsonResponse(response_data)
+    except:
+      return JsonResponse({
+        'message': 'Apenas usuários autenticados podem visualizar o saldo.'
+      },
+      status=403)
+      
