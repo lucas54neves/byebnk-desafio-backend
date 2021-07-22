ca1321f3eee78436ff89f6e0395ae42c946c762f
Lucas Neves <lucas54neves@gmail.com>
2021-07-22 02:16:42 -0300

    add user test

--- src/users/tests.py
+++ src/users/tests.py
@@ -1,3 +1,38 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from users.models import User
 
-# Create your tests here.
+class UserAPITestCase(APITestCase):
+  def test_signup(self):
+    response = self.client.post('/accounts/signup', {
+      'email':'test@mail.com',
+      'password': '123',
+    }, format='json')
+
+    self.assertRedirects(response, '/accounts/signup/', status_code=301, target_status_code=200)
+  
+  def test_login(self):
+    User.objects.create(
+      email='test@mail.com',
+      password='123456'
+    )
+
+    response = self.client.post('/accounts/login', {
+      'email':'test@mail.com',
+      'password': '123',
+    }, format='json')
+
+    self.assertRedirects(response, '/accounts/login/', status_code=301, target_status_code=200)
+
+  def test_logout(self):
+    user = User.objects.create(
+      email='test@mail.com',
+      password='123456'
+    )
+    
+    self.client.force_authenticate(user=user)
+
+    response = self.client.post('/accounts/logout/', {'revoke_token': 'false'}, format='json')
+    
+    self.assertEqual(response.status_code, 200)
+    
