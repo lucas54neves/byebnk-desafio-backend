3f6ac935baf0976c9ffc6f92082ce74d3b77c850
Lucas Neves <lucas54neves@gmail.com>
2021-07-22 02:16:22 -0300

    update asset test

--- src/assets/tests.py
+++ src/assets/tests.py
@@ -1,19 +1,21 @@
+from rest_framework.test import APITestCase
 from django.test import TestCase
 from assets.models import Assets
+from users.models import User
 
 class AssetTestCase(TestCase):
   def setUp(self):
     self.asset = Assets.objects.create(
-      name='Bitcoin',
-      modality='CR',
+      name='BTC-USD',
+      modality='cripto',
       value=120
     )
   
   def test_get(self):
     asset = Assets.objects.get(pk=self.asset.id)
 
-    self.assertEqual(asset.name, 'Bitcoin')
-    self.assertEqual(asset.modality, 'CR')
+    self.assertEqual(asset.name, 'BTC-USD')
+    self.assertEqual(asset.modality, 'cripto')
     self.assertEqual(asset.value, 120)
   
   def test_update(self):
@@ -38,3 +40,34 @@ class AssetTestCase(TestCase):
     with self.assertRaises(Assets.DoesNotExist):
       Assets.objects.get(pk=self.asset.id)
 
+class AssetAPITestCase(APITestCase):
+  def test_get(self):
+    user = User.objects.create(
+      email='test@mail.com',
+      password='123456'
+    )
+    
+    self.client.force_authenticate(user=user)
+
+    Assets.objects.create(
+      name='BTC-USD',
+      modality='cripto',
+      value=120
+    )
+
+    Assets.objects.create(
+      name='PETR4.SA',
+      modality='variable',
+      value=12
+    )
+
+    Assets.objects.create(
+      name='XPLG11.SA',
+      modality='variable',
+      value=10
+    )
+
+    response = self.client.get('/assets/', format='json')
+
+    self.assertEqual(len(response.data), 3)
+  
